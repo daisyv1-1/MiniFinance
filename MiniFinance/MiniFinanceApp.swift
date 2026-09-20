5e19424f712eb666444f5b294d2adc19072f230a
Daisy <daisyv1104@gmail.com>
2026-09-20 16:29:40 +0530

    Initial Commit

---
+++ MiniFinance/MiniFinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniFinanceApp.swift
+//  MiniFinance
+//
+//  Created by daisy on 20/09/26.
+//
+
+import SwiftUI
+
+@main
+struct MiniFinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
